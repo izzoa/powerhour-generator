e2e2c990882e00d2e24ac3a8349eb786482ffbae
Anthony <izzo.anthony@gmail.com>
2024-03-01 14:37:55 -0800

    Update powerhour_generator.py

--- powerhour_generator.py
+++ powerhour_generator.py
@@ -11,7 +11,8 @@ def draw_progress_bar(progress, total, prefix='', length=50, fill='█', print_e
     filled_length = int(length * progress // total)
     bar = fill * filled_length + '-' * (length - filled_length)
     print(f'\r{prefix} |{bar}| {percent}% Complete', end=print_end)
-    sys.stdout.flush()  # Flush the buffer
+    # Flush the buffer
+    sys.stdout.flush()
 
 def run_command(command, log_file_path):
     with open(log_file_path, 'w') as log_file:
@@ -51,8 +52,12 @@ def reencode_videos(video_file, start_time, duration, output_path, log_file_path
     fade_in_start = 0  # Start fade in at the beginning of the video
     fade_out_start = max(duration - fade_duration, 0)  # Start fade out fade_duration seconds before the end
 
-    # Audio filters
-    audio_filters = f"loudnorm=I=-23:LRA=7:TP=-1.5:measured_I={audio_loudness['input_i']}:measured_LRA={audio_loudness['input_lra']}:measured_TP={audio_loudness['input_tp']}:measured_thresh={audio_loudness['input_thresh']}:offset={audio_loudness['target_offset']}:linear=true:print_format=summary"
+    if audio_loudness:
+        # Audio filters with loudness normalization
+        audio_filters = f"loudnorm=I=-23:LRA=7:TP=-1.5:measured_I={audio_loudness['input_i']}:measured_LRA={audio_loudness['input_lra']}:measured_TP={audio_loudness['input_tp']}:measured_thresh={audio_loudness['input_thresh']}:offset={audio_loudness['target_offset']}:linear=true:print_format=summary"
+    else:
+        # Skip loudness normalization if audio_loudness is empty
+        audio_filters = "anull"
 
     ffmpeg_command = [
         'ffmpeg', '-y', '-ss', str(start_time), '-t', str(duration), '-i', video_file,
@@ -89,7 +94,6 @@ def main(video_folder, common_clip, fade_duration, output_file):
         loudness_results = {}
         for i, video_file in enumerate(video_files):
             duration = get_video_duration(video_file)
-            # Ensure video is at least 80 seconds to allow for a 60-second clip and 10-second buffer on both ends
             if duration and duration >= 80:
                 loudness_info = analyze_loudness(video_file, os.path.join(ffmpeg_logs_dir, f'loudness_{i:04d}.log'))
                 if loudness_info:
@@ -104,7 +108,7 @@ def main(video_folder, common_clip, fade_duration, output_file):
 
         clip_list, failed = [], 0
         for i, (video_file, (duration, loudness_info)) in enumerate(loudness_results.items(), start=1):
-            start_time = random.randint(10, int(duration) - 70)  # Adjusted start_time selection
+            start_time = random.randint(10, int(duration) - 70)
             temp_clip_name = f'temp_clip_{i:04d}.mp4'
             temp_clip_path = os.path.join(temp_dir, temp_clip_name)
 
