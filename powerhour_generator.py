801535f82062364b43c8392499a1abf2681bd653
Anthony <izzo.anthony@gmail.com>
2024-03-04 07:38:34 -0800

    fixed analyze_loudness function

--- powerhour_generator.py
+++ powerhour_generator.py
@@ -3,21 +3,27 @@ import sys
 import shutil
 import random
 import subprocess
+import json
 from glob import glob
 from tempfile import TemporaryDirectory
+from datetime import datetime
 
 def draw_progress_bar(progress, total, prefix='', length=50, fill='█', print_end="\r"):
     percent = "{0:.1f}".format(100 * (progress / float(total)))
     filled_length = int(length * progress // total)
     bar = fill * filled_length + '-' * (length - filled_length)
     print(f'\r{prefix} |{bar}| {percent}% Complete', end=print_end)
-    # Flush the buffer
     sys.stdout.flush()
 
 def run_command(command, log_file_path):
-    with open(log_file_path, 'w') as log_file:
-        result = subprocess.run(command, stderr=log_file, stdout=subprocess.DEVNULL)
-    return result.returncode == 0
+    try: 
+        with open(log_file_path, 'w') as log_file:
+            result = subprocess.run(command, stderr=log_file, stdout=subprocess.DEVNULL)
+        return result.returncode == 0
+    except Exception as e:
+        with open(log_file_path, 'a') as log_file:
+            log_file.write(str(datetime.now()) + " : " + str(e) + " : " + str(command))
+        return False
 
 def get_video_duration(video_file):
     try:
@@ -30,34 +36,72 @@ def get_video_duration(video_file):
         print(f"Error getting duration for {video_file}")
         return None
 
-def analyze_loudness(video_file, log_file_path):
+def analyze_loudness(video_file, log_file_path, json_output_dir):
     ffmpeg_command = [
         'ffmpeg', '-i', video_file, '-af', 'loudnorm=I=-23:LRA=7:print_format=json', '-f', 'null', '-'
     ]
+
     try:
-        result = subprocess.check_output(ffmpeg_command, stderr=subprocess.STDOUT, text=True)
-        loudness_info = {}
-        for line in result.split('\n'):
-            if ':' in line:
-                key, value = line.strip().split(':', 1)
-                loudness_info[key.strip()] = value.strip()
-        return loudness_info
+        # Execute the ffmpeg command and capture the output
+        output = subprocess.check_output(ffmpeg_command, stderr=subprocess.STDOUT, text=True)
+        loudness_info_objects = []  # List to store all captured JSON objects
+        json_data_accumulated = ""  # String to accumulate JSON data
+        capturing_json = False  # Flag to indicate when to start capturing JSON data
+
+        for line in output.splitlines():
+            if '{' in line:
+                capturing_json = True  # Start capturing JSON data
+                json_data_accumulated = line[line.find('{'):]  # Start from '{'
+            elif capturing_json:
+                json_data_accumulated += line
+                if '}' in line:
+                    try:
+                        json_obj = json.loads(json_data_accumulated)
+                        loudness_info_objects.append(json_obj)  # Add the JSON object to the list
+                        capturing_json = False  # Reset for the next JSON object
+                        json_data_accumulated = ""  # Clear for the next accumulation
+                    except json.JSONDecodeError:
+                        # In case of a JSON decoding error, just reset and continue
+                        capturing_json = False
+                        json_data_accumulated = ""
+
+        # Filter the captured JSON objects based on the expected keys
+        expected_keys = {"input_i", "input_tp", "input_lra", "input_thresh",
+                         "output_i", "output_tp", "output_lra", "output_thresh",
+                         "normalization_type", "target_offset"}
+
+        loudness_info = next((obj for obj in loudness_info_objects if set(obj.keys()) == expected_keys), None)
+
+        if loudness_info is None:
+            raise ValueError("No matching JSON loudness data found in ffmpeg output.")
+
+        # Save the loudness information to a file
+        json_file_name = os.path.basename(video_file) + '_loudness.json'
+        json_file_path = os.path.join(json_output_dir, json_file_name)
+        with open(json_file_path, 'w') as json_file:
+            json.dump(loudness_info, json_file, indent=4)
+
     except subprocess.CalledProcessError as e:
         print(f"Error analyzing loudness for {video_file}. See {log_file_path}")
         with open(log_file_path, 'w') as log_file:
             log_file.write(str(e.output))
-        return None
+    except ValueError as e:
+        print(f"Error extracting loudness data for {video_file}. See {log_file_path}")
+        with open(log_file_path, 'w') as log_file:
+            log_file.write(str(e))
 
-def reencode_videos(video_file, start_time, duration, output_path, log_file_path, fade_duration, audio_loudness):
-    fade_in_start = 0  # Start fade in at the beginning of the video
-    fade_out_start = max(duration - fade_duration, 0)  # Start fade out fade_duration seconds before the end
+def reencode_videos(video_file, start_time, duration, output_path, log_file_path, fade_duration, json_loudness_file):
+    fade_in_start = 0
+    fade_out_start = max(duration - fade_duration, 0)
 
-    if audio_loudness:
-        # Audio filters with loudness normalization
-        audio_filters = f"loudnorm=I=-23:LRA=7:TP=-1.5:measured_I={audio_loudness['input_i']}:measured_LRA={audio_loudness['input_lra']}:measured_TP={audio_loudness['input_tp']}:measured_thresh={audio_loudness['input_thresh']}:offset={audio_loudness['target_offset']}:linear=true:print_format=summary"
-    else:
-        # Skip loudness normalization if audio_loudness is empty
-        audio_filters = "anull"
+    try:
+        with open(json_loudness_file, 'r') as json_file:
+            audio_loudness = json.load(json_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print(f"There was an error loading the JSON file for {video_file}. Using default loudness values.")
+        audio_loudness = {}
+
+    audio_filters = f"loudnorm=I=-23:LRA=7:TP=-1.5:measured_I={audio_loudness.get('input_i', '-23.0')}:measured_LRA={audio_loudness.get('input_lra', '7.0')}:measured_TP={audio_loudness.get('input_tp', '-1.5')}:measured_thresh={audio_loudness.get('input_thresh', '-50.0')}:offset={audio_loudness.get('target_offset', '0.0')}:linear=true:print_format=summary"
 
     ffmpeg_command = [
         'ffmpeg', '-y', '-ss', str(start_time), '-t', str(duration), '-i', video_file,
@@ -85,7 +129,12 @@ def main(video_folder, common_clip, fade_duration, output_file):
 
     with TemporaryDirectory() as temp_dir:
         ffmpeg_logs_dir = os.path.join(temp_dir, 'logs')
+        loudness_json_dir = os.path.join(temp_dir, 'loudness_json')
         os.makedirs(ffmpeg_logs_dir, exist_ok=True)
+        os.makedirs(loudness_json_dir, exist_ok=True)
+
+        common_clip_log_path = os.path.join(ffmpeg_logs_dir, 'common_clip.log')
+        analyze_loudness(common_clip, common_clip_log_path, loudness_json_dir)
 
         max_videos = 60
         video_files = random.sample(video_files, min(max_videos, len(video_files)))
@@ -95,29 +144,33 @@ def main(video_folder, common_clip, fade_duration, output_file):
         for i, video_file in enumerate(video_files):
             duration = get_video_duration(video_file)
             if duration and duration >= 80:
-                loudness_info = analyze_loudness(video_file, os.path.join(ffmpeg_logs_dir, f'loudness_{i:04d}.log'))
-                if loudness_info:
-                    loudness_results[video_file] = (duration, loudness_info)
+                log_file_path = os.path.join(ffmpeg_logs_dir, f'loudness_{i:04d}.log')
+                analyze_loudness(video_file, log_file_path, loudness_json_dir)
+                loudness_results[video_file] = duration
             draw_progress_bar(i + 1, len(video_files))
         print("\nFinished analyzing loudness and checking durations.")
 
         common_clip_temp = os.path.join(temp_dir, 'common_clip.mp4')
-        if not reencode_videos(common_clip, 0, fade_duration, common_clip_temp, os.path.join(ffmpeg_logs_dir, 'common_clip.log'), fade_duration, {}):
+        common_clip_loudness_json = os.path.join(loudness_json_dir, os.path.basename(common_clip) + '_loudness.json')
+
+        # Make sure to analyze loudness of the common clip as well
+        if not reencode_videos(common_clip, 0, fade_duration, common_clip_temp, os.path.join(ffmpeg_logs_dir, 'common_clip.log'), fade_duration, common_clip_loudness_json):
             print(f"Failed to re-encode common clip: {common_clip}")
             return
 
         clip_list, failed = [], 0
-        for i, (video_file, (duration, loudness_info)) in enumerate(loudness_results.items(), start=1):
+        for i, (video_file, duration) in enumerate(loudness_results.items(), start=1):
             start_time = random.randint(10, int(duration) - 70)
             temp_clip_name = f'temp_clip_{i:04d}.mp4'
             temp_clip_path = os.path.join(temp_dir, temp_clip_name)
+            json_loudness_file = os.path.join(loudness_json_dir, os.path.basename(video_file) + '_loudness.json')
 
-            if reencode_videos(video_file, start_time, 60, temp_clip_path, os.path.join(ffmpeg_logs_dir, f'video_{i:04d}.log'), fade_duration, loudness_info):
+            if reencode_videos(video_file, start_time, 60, temp_clip_path, os.path.join(ffmpeg_logs_dir, f'video_{i:04d}.log'), fade_duration, json_loudness_file):
                 clip_list.append(temp_clip_path)
             else:
                 print(f"Failed to process video: {video_file}")
                 failed += 1
-            draw_progress_bar(i, len(video_files), prefix='Processing: ')
+            draw_progress_bar(i, len(loudness_results), prefix='Processing: ')
 
         print(f"\nProcessing complete. {failed} files failed to process.")
 
@@ -132,14 +185,19 @@ def main(video_folder, common_clip, fade_duration, output_file):
         if not run_command(concat_command, os.path.join(ffmpeg_logs_dir, 'concat.log')):
             print(f"Failed to concatenate videos. See {os.path.join(ffmpeg_logs_dir, 'concat.log')}")
 
+
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        print("Usage: python script_name.py /path/to/video/folder /path/to/common_clip.mp4 fade_duration_in_seconds output_file_name.mp4")
+        print("Usage: python powerhour_generator.py /path/to/video/folder /path/to/common_clip.mp4 fade_duration_in_seconds output_file_name.mp4")
         sys.exit(1)
 
-    video_folder_path = sys.argv[1]
-    common_clip_path = sys.argv[2]
-    fade_duration_seconds = float(sys.argv[3])
-    output_file_name = sys.argv[4]
-
+    try:
+        video_folder_path = sys.argv[1]
+        common_clip_path = sys.argv[2]
+        fade_duration_seconds = float(sys.argv[3])
+        output_file_name = sys.argv[4]
+    except ValueError:
+        print("Error: fade_duration_in_seconds argument must be a number.")
+        sys.exit(1)
+    
     main(video_folder_path, common_clip_path, fade_duration_seconds, output_file_name)
